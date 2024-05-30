216c2c8035bec6787dfeb64d184dda331b653877
eucleiance <eucleiance@gmail.com>
2024-05-30 12:59:33 +0545

    476A

---
+++ A20J_2A/476A_dreamoon_and_stairs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n, m, x;
+  bool check;
+  cin >> n >> m;
+  if (m > n) check = false;
+  x = (n % 2 == 0) ? (n / 2) : (n/2) + 1;
+  while (x%m != 0) x++;
+  cout << (check != false ? x : -1) << endl;
+  return 0;
+}
